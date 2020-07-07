5854085921b287d40cac8b1c1ce06ea19ab4ab33
Egenie Next <egenie.apple@gmail.com>
2020-07-07 15:39:44 +0500

    stack navigation

--- App.js
+++ App.js
@@ -12,40 +12,82 @@ import {
   Text,
   Button,
 } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+import {NavigationContainer} from '@react-navigation/native';
+import { TextInput } from 'react-native-gesture-handler';
 
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-
-    console.log('Here in Use Effect... ');
-  //  this.myTime = setInterval(() => {
-      
-  //   }, 5000);
-
-    return function cleanUp () {
-      console.log('Here is Component Wll UnMounttt.... ');
-      // clearInterval(this.myTime);
-    }
-  }, []);
-  console.log('Here is Custom Render11111');
+// function HomeScreen (props) {
+//   console.log('Here Prossss..... : ', props);
   
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home Screen</Text>
+//       <Button title = {"Button Click Here"} onPress={() => props.navigation.navigate('Profile')} />
+//     </View>
+//   )
+// }
+
+function ProfileScreen () {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile Screen</Text>
+    </View>
+  )
+}
+
+function HomeScreen ({navigation, route}) {
+  React.useEffect(() => {
+    console.log('IIIII I I II  II ');
+    
+    if (route.params?.post) {
+      console.log('J J J J  J  J J J J');
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+      title = "Create post"
+      onPress = {() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{margin: 10}}> POST : {route.params?.post}</Text>
+    </View>
+  )
+}
+
+function CreatePostScreen ({navigation, route}) {
+  const [postText, setPostText] = React.useState('');
   return (
     <>
-    <Text>
-      Here is Test One
-    </Text>
-    <Text style={{margin: 50}}>
-      {`Here is Second ${count}`}
-    </Text>
-
-    <Button 
-    title={'Incease Count'}
-    onPress = {() => setCount(count + 1)}
+    <TextInput
+    multiline
+    placeholder= "What's on your mind?"
+    style={{height: 200, padding: 10, backgroundColor: 'white'}}
+    value= {postText}
+    onChangeText={setPostText}
     />
-     
+
+    <Button
+    title="Done"
+    onPress={() => {
+      navigation.navigate('Home', {post: postText})
+    }}
+    />
     </>
+  )
+}
+
+const App = () => {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component= {HomeScreen} />
+      <Stack.Screen name="Profile" component = {ProfileScreen} options={{title: "Test Name"}}/>
+      <Stack.Screen name="CreatePost" component = {CreatePostScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
