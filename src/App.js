bf9043ed86baf32676f94d099787138622ceedee
Egenie Next <egenie.apple@gmail.com>
2020-07-13 11:51:30 +0500

    basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Navigation from "./Navigation";
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
 
-function App () {
-    return(
-        <Navigation/>
+
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Navigation />
+            </PersistGate>
+        </Provider>
     )
 };
 
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,48 +1,62 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
 import { Text } from 'native-base';
+import { connect } from 'react-redux';
+import { actions } from '../../redux/actions/UserAction'
 import styles from './styles';
 import InputField from '../../components/InputField';
 import CustomButton from '../../components/CustomButton'
-import UserService from '../../services/UserService';
 
 
-async function onPressLoginButton (navigation) {
+async function onPressLoginButton(props) {
   // navigation.navigate('Dashboard');
-  let data = await UserService.doLogin('hamza3', 'hamza3');
-  console.log('Data???? : ', data);
-  
+
+  let dataRedux = await props.login('hamza3', 'hamza3');
+  console.log('dataRedux  : ', dataRedux);
+
 }
 
-function Login({navigation}) {
+function Login(props) {
 
   return (
     <View style={styles.container}>
       <View style={{ width: '90%' }}>
         <InputField
-          placeholder = "UserName"
-          icon = "person-outline"
+          placeholder="UserName"
+          icon="person-outline"
         />
         <InputField
           placeholder="Password"
-          icon = "lock-closed-outline"
+          icon="lock-closed-outline"
         />
       </View>
-        <CustomButton 
-          text={"Login"}
-          style={styles.customButton}
-          onPressLoginButton = {() => onPressLoginButton(navigation)}
-          />
+      <CustomButton
+        text={"Login"}
+        style={styles.customButton}
+        onPressLoginButton={() => onPressLoginButton(props)}
+      />
 
-          <View style={styles.signupTextContainer}>
-          <Text>Don't have account? </Text>
-          <TouchableOpacity
-          onPress={() => navigation.navigate('SignUp')}
-          >
+      <View style={styles.signupTextContainer}>
+        <Text>Don't have account? </Text>
+        <TouchableOpacity
+          onPress={() => props.navigation.navigate('SignUp')}
+        >
           <Text style={styles.signupText}>SignUp</Text>
-          </TouchableOpacity>
-          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
-export default Login;
+
+const mapStateToProps = state => {
+  return {
+    userObject: state.user.userObject,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: (userName, password) => dispatch(actions.setLogin(userName, password)),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
